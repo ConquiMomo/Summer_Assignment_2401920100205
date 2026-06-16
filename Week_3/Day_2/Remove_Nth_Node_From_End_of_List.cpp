821b884a0c728e4f0dabe7e265e3c41920274a07
Delta Student <mohitmehra654321@gmail.com>
2026-06-16 16:57:23 +0530

    Week 3 Day 2 Solutions

---
+++ Week_3/Day_2/Remove_Nth_Node_From_End_of_List.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverse(ListNode* head) {
+        if (head == NULL || head->next == NULL) {
+            return head;
+        }
+        ListNode* newHead = reverse(head->next);
+        ListNode* front = head->next;
+        front->next = head;
+        head->next = NULL;
+        return newHead;
+    }
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* fast = head;
+        ListNode* slow = head;
+        for (int i = 0; i < n; i++) {
+            fast = fast->next;
+        }
+
+        if (fast == NULL)
+            return head->next;
+
+        while (fast->next != NULL) {
+            slow = slow->next;
+            fast = fast->next;
+        }
+
+        ListNode* delnode = slow->next;
+        slow->next = slow->next->next;
+        delete delnode;
+        return head;
+    }
+};
